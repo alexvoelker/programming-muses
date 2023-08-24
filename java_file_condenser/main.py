58e7c0bbb6ce287f3d0aca0d8f7b5942c7ba7725
Alex <25597637+alexvoelker@users.noreply.github.com>
2023-08-23 20:46:04 -0400

    update java file condenser

--- java_file_condenser/main.py
+++ java_file_condenser/main.py
@@ -1,3 +1,5 @@
+import os
+
 file_name = input("Enter a file name in this directory to condense into one line: ")
 
 try:
@@ -7,7 +9,7 @@ try:
             contents += line[:len(line) - 1].strip()
     
     print(f"contents: {contents}")
-
+    os.system(f"touch out.{file_name}")
     with open(f"out.{file_name}", 'w') as out_file:
         out_file.write(contents)
     print(f"Data written to out.{file_name}")
