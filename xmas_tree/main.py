d9ac7f99e653c1c1bc165043ef5f2759c201fb3a
Alex <25597637+alexvoelker@users.noreply.github.com>
2023-10-23 19:10:26 -0400

    fix some spacing issues in printing

--- xmas_tree/main.py
+++ xmas_tree/main.py
@@ -7,8 +7,20 @@ def join(arr: list[int], s: str) -> str:
 	return out
 
 
+def extract_digit(x: int) -> int:
+	digits = 0
+	if x == 0:
+		return 0
+
+	while x > 0:
+		x = x // 10
+		digits += 1
+	return digits
+
+
 def print_tree(x: int):
-	spaces = (x * 2 - 2) * 2 + 1
+	largest_digit = extract_digit(x)
+	spaces = (largest_digit * x * 2 - 2) * 2 + 1
 	spaces = spaces if spaces > 3 else 3
 
 	for i in range(1, x + 1):
@@ -16,9 +28,8 @@ def print_tree(x: int):
 		for j in range(i - 1, 0, -1):
 			array.insert(0, j)
 			array.append(j)
-		#print(f"i: {i} // {join(array, ' ')}")
 		print(f"{join(array, ' '):^{spaces}}")
 	print(f"{'| |':^{spaces}}")
 
 if __name__ == '__main__':
-	print_tree(5)
+	print_tree(13)
